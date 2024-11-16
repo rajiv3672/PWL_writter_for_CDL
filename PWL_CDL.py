c5f763e12f82d2ba9ecd1b8307c3ce1475563064
rajiv3672 <rajiv.sust.15@gmail.com>
2024-11-16 22:31:46 +0600

    Under development

--- PWL_CDL.py
+++ PWL_CDL.py
@@ -7,7 +7,7 @@ s=1
 F=f; P=p; N=n; U=u; M=m; S=s; V=s; v=s;
 print("f=femto,\n p=pico,\n n=nano,\n u=micro,\n m=mili,\n s=socond\n The inputs are not case sensitive.\n")
 
-rtu = input(str("Enter the unit for rise time:"))
+rtu = input("Enter the unit for rise time:")
 if (rtu=='f' or rtu=='p' or rtu=='u' or rtu=='n' or rtu=='m' or rtu=='s' or rtu=='F' or rtu=='P' or rtu=='U' or rtu=='N' or rtu=='M' or rtu=='S'):
     print("Input is valid")
 else:
@@ -25,9 +25,9 @@ elif (rtu=='m' or rtu =='M'):
 elif (rtu=='s' or rtu =='S'):
     rtu=float(s)
 
-rt=input(float("\nInput the rise time:"))
+rt=float(input("Input the rise time:"))
 
-ftu = input(str("Enter the unit for fall time:"))
+ftu = input("Enter the unit for fall time:")
 if (ftu=='f' or ftu=='p' or ftu=='u' or ftu=='n' or ftu=='m' or ftu=='s' or ftu=='F' or ftu=='P' or ftu=='U' or ftu=='N' or ftu=='M' or ftu=='S'):
     print("Input is valid")
 else:
@@ -45,8 +45,30 @@ elif (ftu=='m' or ftu =='M'):
 elif (ftu=='s' or ftu =='S'):
     ftu=float(s)
 
-ft=input(float("\nInput the fall time:"))
+ft=float(input("\nInput the fall time:"))
+pw=float(input("Enter the unit for time of the pulse:"))
 
-vu=input(str("Input the umit for the voltage. \n V for volt, m for milivolt.\n"))
-hvv=input(float("Input the High level value for the PWL:"))
-lvv=input(float("Input the Low level value for the PWL:"))
+vu=input("Input the umit for the voltage. \n V for volt, m for milivolt.\n")
+hvv=float(input("Input the High level value for the PWL:"))
+lvv=input("Input the Low level value for the PWL:")
+
+print("Enter he time pairs for the duration when the signal is high.\nThe code will automatically calculate the low time duration.")
+
+ht1=[]
+ht2=[]
+
+i=1
+while True:
+    print("Pair {}, high time 1:\nEner E or e to stop".format(i))
+    hx1=input()
+    if (hx1 == 'E' or hx1 == 'e'):
+        break
+    ht1.append(float(hx1))
+    print("Pair {}, high time 2:".format(i))
+    hx2=input()
+    if (hx2 == 'E' or hx2 == 'e'):
+        break
+    ht2.append(float(hx2))
+    i=i+1
+
+    
