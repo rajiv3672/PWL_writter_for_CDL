e1b778b1dad3a20605b1cb79fa8428673df1662a
rajiv3672 <rajiv.sust.15@gmail.com>
2024-11-20 22:24:04 +0600

    Cleared up redundancy
    I cleared up some redundancy with the suggestion of my colleague.

--- PWL_CDL.py
+++ PWL_CDL.py
@@ -5,72 +5,44 @@ result=[]
 
 print("f=femto,\n p=pico,\n n=nano,\n u=micro,\n m=mili,\n s=socond\n The inputs are NOT case sensitive.\n")
 
-rtu = input("Enter the UNIT for rise time:")
-#This part sets the float values to the string inputs.
-if (rtu=='f' or rtu=='p' or rtu=='u' or rtu=='n' or rtu=='m' or rtu=='s' or rtu=='F' or rtu=='P' or rtu=='U' or rtu=='N' or rtu=='M' or rtu=='S'):
-    print("Input is valid")
-else:
-    print("Input is invalid")
-if (rtu=='f' or rtu=='F'):
-    rtu=float(f)
-elif (rtu=='p' or rtu=='P'):
-    rtu=float(p)
-elif (rtu=='n' or rtu=='N'):
-    rtu=float(n)
-elif (rtu=='u' or rtu =='U'):
-    rtu=float(u)
-elif (rtu=='m' or rtu =='M'):
-    rtu=float(m)
-elif (rtu=='s' or rtu =='S'):
-    rtu=float(s)
 
+def var_assign(X):#This part sets the float values to the string inputs.
+    
+    if (X=='f' or X=='p' or X=='u' or X=='n' or X=='m' or X=='s' or X=='F' or X=='P' or X=='U' or X=='N' or X=='M' or X=='S'):
+        print("Input is valid")
+    else:
+        print("Input is invalid")
+    if (X=='f' or X=='F'):
+        X=float(f)
+    elif (X=='p' or X=='P'):
+        X=float(p)
+    elif (X=='n' or X=='N'):
+        X=float(n)
+    elif (X=='u' or X =='U'):
+        X=float(u)
+    elif (X=='m' or X =='M'):
+        X=float(m)
+    elif (X=='s' or X =='S' or X=='V' or X=='v'):
+        X=float(s)
+    return X
+
+rtu = input("Enter the UNIT for rise time:")
+rtu = var_assign(rtu)
 rt=float(input("Input the rise time:"))
 
 ftu = input("Enter the UNIT for fall time:")
-if (ftu=='f' or ftu=='p' or ftu=='u' or ftu=='n' or ftu=='m' or ftu=='s' or ftu=='F' or ftu=='P' or ftu=='U' or ftu=='N' or ftu=='M' or ftu=='S'):
-    print("Input is valid")
-else:
-    print("Input is invalid")
-if (ftu=='f' or ftu=='F'):
-    ftu=float(f)
-elif (ftu=='p' or ftu=='P'):
-    ftu=float(p)
-elif (ftu=='n' or ftu=='N'):
-    ftu=float(n)
-elif (ftu=='u' or ftu =='U'):
-    ftu=float(u)
-elif (ftu=='m' or ftu =='M'):
-    ftu=float(m)
-elif (ftu=='s' or ftu =='S'):
-    ftu=float(s)
-
+ftu = var_assign(ftu)
 ft=float(input("\nInput the fall time:"))
 
 pw=input("Enter the UNIT for time of the pulse:")
-if (pw=='f' or pw=='p' or pw=='u' or pw=='n' or pw=='m' or pw=='s' or pw=='F' or pw=='P' or pw=='U' or pw=='N' or pw=='M' or pw=='S'):
-    print("Input is valid")
-else:
-    print("Input is invalid")
-if (pw=='f' or pw=='F'):
-    pw=float(f)
-elif (pw=='p' or pw=='P'):
-    pw=float(p)
-elif (pw=='n' or pw=='N'):
-    pw=float(n)
-elif (pw=='u' or pw =='U'):
-    pw=float(u)
-elif (pw=='m' or pw =='M'):
-    pw=float(m)
-elif (pw=='s' or pw =='S'):
-    pw=float(s)
+pw = var_assign(pw)
 
-
-vu=input("Input the umit for the voltage. \n V for volt, m for milivolt.\n")
+vu=input("Input the UNIT for the voltage.")
 hvv=float(input("Input the High level value for the PWL:"))
 lvv=float(input("Input the Low level value for the PWL:"))
-if (vu == 'm' or vu== 'M'):
-    hvv=hvv*m
-    lvv=lvv*m
+hvv=hvv*var_assign(vu)
+lvv=lvv**var_assign(vu)
+
 
 td=0
 ins=input("Input the instance's name:")
@@ -80,7 +52,7 @@ result.append(ins)
 result.append(port1)
 result.append(port2)
 result.append('PWL')
-print("Enter he time pairs for the duration when the signal is high.\nThe code will automatically calculate the low time duration.")
+print("Enter he time pairs for the duration when the signal is high.\nThe code will automatically calculate the low time duration.\n PLEASE COMPLETE THE PAIRS")
 
 ht1=[]
 ht2=[]
@@ -116,11 +88,11 @@ while j<i:
 if (ht1[0] != 0):
     lt1.pop(len(lt1)-1)
 elif (ht1[0] == 0):
-    lt2.append(lt1[len(lt1)])
+    lt2.append(lt1[len(lt1)-1])
 
 k=0
 while True:
-    if ((k+1)>=i):
+    if (k>=i):
         break
     if (lt1[0]<ht1[0]):
         result.append(lt1[k])
@@ -142,6 +114,14 @@ while True:
         result.append(lt2[k])
         result.append(lvv)
     k=k+1
+
+if (ht1[0] == 0):#pops out two redundant valuse
+    result.pop(len(result)-1)
+    result.pop(len(result)-1)
+
+if (ht1[0] != 0):#APPENDS EXTRA VALUE FOR ENDING THE PWL VALUE IN ZERO AS IT IS PREFFERRED FOR SIMULATION
+    result.append(ht2[(len(ht2)-1)]+ftu)
+    result.append(0)
 
 result.append('TD=0')
 
